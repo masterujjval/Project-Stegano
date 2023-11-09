dafb6ff97b72165054d8094a166a16c1897b0ff8
masterujjval <guptarickey3@gmail.com>
2023-11-10 00:27:09 +0530

    changes made

--- decrypt-msg.py
+++ decrypt-msg.py
@@ -1,13 +1,15 @@
 import random
 import string
 import cryptography
+from colorama import Fore,Style
 
 from cryptography.fernet import Fernet
 
-enc_msg=input("Enter the Encrypted message: ")
-f_obj=input("Enter the key: ")
+enc_msg=input(Fore.GREEN + "Enter the Encrypted message: "+ Fore.YELLOW)
+f_obj=input(Fore.RED + "\nEnter the key: "+ Fore.GREEN)
 
 #encoding to string to bytes
+print(Fore.RED)
 test_msg=enc_msg.encode('utf-32')
 
 test_key=f_obj.encode('utf-32')
@@ -16,6 +18,7 @@ test_key=Fernet(f_obj)
 
 
 de_msg=test_key.decrypt(test_msg)
+
 print(de_msg)
 
 
--- encrypt-msg.py
+++ encrypt-msg.py
@@ -1,21 +1,22 @@
 import random
 import string
 import cryptography
+from colorama import Fore,Style
 
 from cryptography.fernet import Fernet
 
-plain_text=input("Enter text to encrypt: ")
+plain_text=input(Fore.GREEN + "Enter text to encrypt: "+ Fore.YELLOW)
 plain_text=plain_text.encode()
 key=Fernet.generate_key()
 
-print("Save this key for decrypting the message: ")
+print(Fore.GREEN + "Save this key for decrypting the message: " + Fore.RED)
 
 print(key)
 
 f_obj=Fernet(key)
 
 cipher_text=f_obj.encrypt(plain_text)
-print("Encrypted text : ")
+print(Fore.GREEN + "\nEncrypted text : "+ Fore.YELLOW)
 print(cipher_text)
 
 
